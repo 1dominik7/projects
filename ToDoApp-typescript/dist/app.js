e041b8100326a2bc8b0554738f6f493cb5a2fd3c
1dominik7 <111517587+1dominik7@users.noreply.github.com>
2023-05-26 20:14:49 +0200

    ToDoApp-TypeScript

---
+++ ToDoApp-typescript/dist/app.js
@@ -0,0 +1,31 @@
+import { Task, Category } from "./types/types.js";
+import renderTasks from "./helpers/render-tasks.helper.js";
+import { render as renderCategories } from "./helpers/render-categories.helper.js";
+const taskNameInputElement = document.querySelector("#name");
+const addButtonElement = document.querySelector("button");
+const tasksContainerElement = document.querySelector(".tasks");
+const categoriesContainerElement = document.querySelector(".categories");
+let selectedCategory;
+const categories = [
+    Category.GENERAL,
+    Category.WORK,
+    Category.GYM,
+    Category.HOBBY,
+    Category.SOCIAL,
+];
+const tasks = [];
+const addTask = (task) => {
+    tasks.push(task);
+};
+const updateSelectedCategory = (newCategory) => {
+    selectedCategory = newCategory;
+};
+addButtonElement.addEventListener("click", (event) => {
+    event.preventDefault();
+    const newTask = new Task(taskNameInputElement.value, false, selectedCategory);
+    addTask(newTask);
+    newTask.logCreationDate("!!!");
+    renderTasks(tasks, tasksContainerElement);
+});
+renderCategories(categories, categoriesContainerElement, updateSelectedCategory);
+renderTasks(tasks, tasksContainerElement);
